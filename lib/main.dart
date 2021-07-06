71e211a8e15cbb79d7ad99c8865ff0e7e3d4ed79
deivi320 <deibi.ocampo@sofka.com.co>
2021-07-06 15:29:19 -0500

    ejemplo shimmer

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import 'custom_widget.dart';
+import 'movie_data.dart';
+import 'movie_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shimmer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<MovieModel> movies = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    loadData();
+  }
+
+  Future loadData() async {
+    setState(() {
+      isLoading = true;
+    });
+    await Future.delayed(Duration(seconds: 2));
+    movies = List.of(allMovies);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.teal,
+        title: Text("Shimmer Animation Effect"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: loadData)
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: isLoading? 5: movies.length,
+          itemBuilder: (context, index) {
+            if (isLoading) {
+              return buildMovieShimmer();
+            } else {
+              final movie = movies[index];
+              return buildMovieList(movie);
+            }
+          }
+      ),
+
+    );
+  }
+
+  Widget buildMovieList(MovieModel model) =>
+      ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundImage: NetworkImage(model.urlImg),
+        ),
+        title: Text(model.title, style: TextStyle(fontSize: 16),),
+        subtitle: Text(
+          model.detail, style: TextStyle(fontSize: 14), maxLines: 1,),
+      );
+
+  Widget buildMovieShimmer() =>
+      ListTile(
+        leading: CustomWidget.circular(height: 64, width: 64),
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: CustomWidget.rectangular(height: 16,
+            width: MediaQuery.of(context).size.width*0.3,),
+        ),
+        subtitle: CustomWidget.rectangular(height: 14),
+      );
+
+}
---
+++ lib/movie_model.dart
@@ -0,0 +1,10 @@
+class MovieModel {
+  final String urlImg;
+  final String title;
+  final String detail;
+
+
+  const MovieModel({this.urlImg, 
+     this.title, 
+     this.detail});
+}
